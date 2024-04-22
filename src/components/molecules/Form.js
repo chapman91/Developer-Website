031a31864c597983e5db6bac3847dbac6c1a47e2
chapman91 <chapmandoneil91@gmail.com>
2024-04-22 13:10:49 -0400

    gmail readded protonmail uses a different protocol for cummunication

--- src/components/molecules/Form.js
+++ src/components/molecules/Form.js
@@ -55,7 +55,7 @@ const Form = () => {
 
             // Send form data to your server
             // You can use fetch or any other library to submit form data
-            fetch('https://formsubmit.co/webdevdoneil@proton.me', {
+            fetch('https://formsubmit.co/chapmandoneil91@gmail.com', {
                 method: 'POST',
                 body: formDataToSend,
             })
