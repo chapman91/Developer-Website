f8aa92d1f3e930de063b37c5f6adfe31c57d22e3
chapman91 <chapmandoneil91@gmail.com>
2024-04-26 18:32:34 -0400

    added two articles picture and description

--- src/components/organisms/BlogSection.js
+++ src/components/organisms/BlogSection.js
@@ -3,6 +3,10 @@ import "./BlogSection.css"
 import BlogCard from "../molecules/BlogCard"
 import Container from 'react-bootstrap/Container';
 import "./BlogSection.css"
+import imageI from "../../assets/images/blockchainarticle1.jpg"
+import imageII from "../../assets/images/stuxnet.jpg"
+
+
 
 export const BlogSection = () => {
   return (
@@ -12,14 +16,15 @@ export const BlogSection = () => {
       <h1 className="display-4">Blog</h1>
     </div>
     <BlogCard
-     title="Article 1"
-     text="This is a description of the first article."
-     imageSrc="article1.jpg"
+     title="What is Cryptocurrency & Blockchain"
+     text="Demystifying Cryptocurrency and Blockchain: A Beginner's Guide to Understanding Digital Money and Decentralized Ledgers."
+     imageSrc={imageI}
+
     />
     <BlogCard
-    title="Article 2"
-    text="This is a description of the two article."
-    imageSrc="article2.jpg"
+    title="Unraveling Stuxnet: The Cyber Attack That Rewrote the Rules of Warfare"
+    text="Unveiling the Groundbreaking Cyber Assault That Transcended the Digital Realm."
+    imageSrc={imageII}
     />
     <BlogCard
     title="Article 3"
